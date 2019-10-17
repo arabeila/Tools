845c7815ca6b3ff9368a69abce311d2b3ce34a39
qiu <1337241933@qq.com>
2019-10-17 17:20:48 +0800

    优化菜单生成

--- src/Services/MenuService.php
+++ src/Services/MenuService.php
@@ -9,7 +9,7 @@
 namespace Arabeila\Tools\Services;
 
 use Arabeila\Tools\Supports\Help;
-use Arabeila\Tools\Models\Menu as AdminMenu;
+use Arabeila\Tools\Models\Menu as MenuModel;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Spatie\Menu\Html;
@@ -28,6 +28,42 @@ class MenuService
         Cache::tags(self::getTags($guard))->flush();
     }
 
+    public function generate($data,$menu,$guard)
+    {
+        if (empty($data['children'])) {
+            $icon = $data['icon'] ? '<i class="'.$data['icon'].'"></i>' : '';
+
+            $text = '<span>'.$data['name'].'</span>';
+
+            $link = Link::to($data['url'], $icon.$text)
+                ->setAttribute('target', $data['type']);
+
+            return $menu->addIf($this->checkPermission($guard,$data['url']),$link);
+        }
+
+        $child = Menu::new()->addClass('treeview-menu');
+
+        $icon = $data['icon'] ? '<i class="'.$data['icon'].'"></i>' : '';
+
+        $text = '<span>'.$data['name'].'</span>';
+
+        $btn = '<span class="pull-right-container">
+						<i class="fa fa-angle-left pull-right"></i>
+					</span>';
+
+        $child->addParentClass('treeview');
+
+        foreach ($data['children'] as $childData) {
+            $child = $this->generate($childData, $child,$guard);
+        }
+
+        if($child->count()){
+            $menu->submenu(Link::to('#', $icon.$text.$btn), $child);
+        }
+
+        return $menu;
+    }
+
     public function build($guard)
     {
         $key = Help::key('menus',Auth::guard($guard)->id());
@@ -40,61 +76,10 @@ class MenuService
         echo Cache::tags(self::getTags())->rememberForever($key, function () use ($guard) {
             $menu = Menu::new()->add(Html::raw('菜单')->addParentClass('header'))->addClass('sidebar-menu')->setAttributes(['data-widget' => 'tree']);
 
-            $menus = AdminMenu::guardName($guard)->with('child')->where('parent_id',0)->get()->toArray();
+            $menus = MenuModel::guardName($guard)->with('children')->where('parent_id',0)->get()->toArray();
 
             foreach ($menus as $key => $value) {
-                if ($value['child'] != null) {
-                    $child = Menu::new()->addClass('treeview-menu');
-
-                    for ($i = 0; $i < count($value['child']); $i++) {
-
-                        if ($value['child'][$i]['icon'] != null) {
-                            $icon = '<i class="'.$value['child'][$i]['icon'].'"></i>';
-                        } else {
-                            $icon = '';
-                        }
-
-                        $text = '<span>'.$value['child'][$i]['name'].'</span>';
-
-                        $link = Link::to($value['child'][$i]['url'], $icon.$text)
-                            ->setAttribute('target', $value['type']);
-
-                        $child->addIf($this->checkPermission($guard, $value['child'][$i]['url']), $link);
-                    }
-
-                    $child->addParentClass('treeview');
-
-                    if ($value['icon'] != null) {
-                        $icon = '<i class="'.$value['icon'].'"></i>';
-                    } else {
-                        $icon = '';
-                    }
-
-                    $text = '<span>'.$value['name'].'</span>';
-
-                    $btn = '<span class="pull-right-container">
-						<i class="fa fa-angle-left pull-right"></i>
-					</span>';
-
-                    if ($child->count()) {
-                        $menu->submenu(Link::to('#', $icon.$text.$btn), $child);
-                    }
-                } else {
-                    if ($value['icon'] != null) {
-                        $icon = '<i class="'.$value['icon'].'"></i>';
-                    } else {
-                        $icon = '';
-                    }
-                    $text = '<span>'.$value['name'].'</span>';
-
-                    $link = Link::to($value['url'], $icon.$text)
-                        ->setAttribute('target', $value['type']);
-
-                    if (!$value['parent_id']) {
-
-                        $menu->addIf($this->checkPermission($guard, $value['url'], '/'), $link);
-                    }
-                }
+                $menu = $this->generate($value, $menu,$guard);
             }
 
             return $menu;
