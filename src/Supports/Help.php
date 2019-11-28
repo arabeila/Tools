cdaf86784a8b888fc9a0db6f8de55c8637084fd6
TELstatic <telstatic@gmail.com>
2019-11-28 15:38:35 +0800

    添加配置文件修改函数

--- src/Supports/Help.php
+++ src/Supports/Help.php
@@ -49,4 +49,29 @@ class Help
         return strtolower(config('app.name').'-'.$guard.'-'.$object.'-'.$id);
     }
 
+
+    function xenv($data)
+    {
+        $envPath = base_path().DIRECTORY_SEPARATOR.'.env';
+        $contentArray = collect(file($envPath, FILE_IGNORE_NEW_LINES));
+
+        $contentArray->transform(function ($item) use ($data) {
+            foreach ($data as $key => $value) {
+                if (str_contains($item, $key)) {
+                    return $key.'='.$value;
+                }
+            }
+            return $item;
+        });
+
+        foreach ($data as $key => $value) {
+            if (env($key) == null) {
+                $contentArray[] = $key.'='.$value;
+            }
+        }
+
+        $content = implode($contentArray->toArray(), PHP_EOL);
+
+        \File::put($envPath, $content);
+    }
 }
